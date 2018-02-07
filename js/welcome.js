418b4e0000474aa7000efa183f4c97360637b827
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2018-02-07 16:21:29 +0100

    Always show the modal

--- js/welcome.js
+++ js/welcome.js
@@ -1,22 +1,7 @@
 // Welcome info popup box
 $(function() {
-  // Check if the user does not want to see the box again
-  if (Cookies.get('dont_show_again')) {
-    return
-  }
-
   // Fade in the modal
   $('#welcome.modal').modal({
     fadeDuration: 250
   })
-
-})
-
-// Saves user preference on modal appearances and closes the existing one
-$(document).on('click', '.dont_show_again', function() {
-  Cookies.set('dont_show_again', true, { expires: 30 })
-
-  $.modal.close()
-
-  return false
 })
