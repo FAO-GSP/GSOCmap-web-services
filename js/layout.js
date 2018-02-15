6fe9fad2cbb1ef783e05c886d4c9fa709201d492
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2018-02-15 12:04:59 +0000

    Tabs in sidebar

--- js/layout.js
+++ js/layout.js
@@ -1,6 +1,22 @@
 $(function() {
+  // Clicking a sidebar control opens the sidebar with that button's target tab
+  // active.
   $('[data-toggle="offcanvas"]').on('click', function () {
-    $('#sidebar').toggleClass('open')
+    let tab = $($(this).data('target'))
+    let sidebar = $('#sidebar')
+
+    if(!sidebar.hasClass('open')) {
+      // If the sidebar is closed, open it and open the tab.
+      tab.add(sidebar).addClass('open')
+    } else if(tab.hasClass('open')) {
+      // If the sidebar is open and the tab active, hide everything.
+      $('#sidebar, #sidebar .tab').removeClass('open')
+    } else {
+      // If the sidebar is open and the tab is not active, just toggle every tab.
+      $('#sidebar .tab').toggleClass('open')
+    }
+
+    // Remove focus on control
     this.blur()
   })
 })
