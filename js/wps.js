25ad8fdeb799875f64353f160c837ab0207c3ff1
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2018-02-15 14:35:02 +0000

    Working statistics wps process

--- js/wps.js
+++ js/wps.js
@@ -1,20 +1,16 @@
 // Request a gs:CropCoverage process
 var crop = function(bbox) {
-  var points = `${bbox[0]} ${bbox[3]}, ${bbox[2]} ${bbox[3]}, ${bbox[2]} ${bbox[1]}, ${bbox[0]} ${bbox[1]}, ${bbox[0]} ${bbox[3]}`
-
-  var template = $('#crop-template').html()
-
-  Mustache.parse(template)
-
-  var rendered = Mustache.render(template, {
-    points: points
+  var request = prepareRequest('crop', {
+    points: bboxToPoints(bbox).map(function(p) {
+      return `${p[0]} ${p[1]}`
+    }).join(', ')
   })
 
   $.ajax({
     url: '/geoserver/GSOC/wms',
     type: 'POST',
     contentType: 'text/plain',
-    data: rendered,
+    data: request,
     // Treat response as a binary file
     xhrFields: {
       responseType: 'blob'
@@ -33,9 +29,52 @@ var crop = function(bbox) {
   })
 }
 
+// Request a ras:RasterZonalStatistics process
+var statistics = function(bbox) {
+  request = prepareRequest('statistics', {
+    points: bboxToPoints(bbox).map(function(p) {
+      return `[${p.join(', ')}]`
+    }).join(', ')
+  })
+
+  $.ajax({
+    url: '/geoserver/GSOC/wms',
+    type: 'POST',
+    contentType: 'text/plain',
+    data: request,
+    dataType: 'xml'
+  }).done(function(data, status, xhr) {
+    // Get statistics values
+    console.log('sum: ' + $(data).find('feature\\:sum').text())
+    console.log('min: ' + $(data).find('feature\\:min').text())
+    console.log('max: ' + $(data).find('feature\\:max').text())
+  }).fail(function(xhr, status, error) {
+    console.log(status + ': ' + error)
+  })
+}
+
+// Converts a bbox to an array of points usable in templates
+var bboxToPoints = function(bbox) {
+  return [
+    [bbox[0], bbox[3]],
+    [bbox[2], bbox[3]],
+    [bbox[2], bbox[1]],
+    [bbox[0], bbox[1]],
+    [bbox[0], bbox[3]]
+  ]
+}
+
+var prepareRequest = function(process, data) {
+  var template = $(`#${process}-template`).html()
+
+  // TODO Trigger parsing on draw end (before button clicked)
+  Mustache.parse(template)
+
+  return Mustache.render(template, data)
+}
+
 // Extract the filename from Content-Disposition header
 var getFilename = function(disposition) {
-
   if (disposition && disposition.indexOf('attachment') !== -1) {
     var filenameRegex = /filename[^;=\n]*=((['"]).*?\2|[^;\n]*)/
     var matches = filenameRegex.exec(disposition)
