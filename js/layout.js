346478f90de9f8acd8676f25d3b8691acd347959
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2018-02-16 12:08:49 +0000

    Keep layout code together addendum

--- js/layout.js
+++ js/layout.js
@@ -13,15 +13,16 @@ $(function() {
   $('[data-toggle="offcanvas"]').on('click', function (e, context) {
     let tab = $($(this).data('target'))
     let sidebar = $('#sidebar')
+    let forced = context && context.forced
 
     if(!sidebar.hasClass('open')) {
       // If the sidebar is closed, open it and open the tab.
       tab.add(sidebar).addClass('open')
       tab.trigger('opened')
-    } else if(tab.hasClass('open') && !context.forced) {
+    } else if(tab.hasClass('open') && !forced) {
       // If the sidebar is open and the tab active, hide everything.
       $('#sidebar, #sidebar .tab').removeClass('open')
-    } else if(!context.forced) {
+    } else if(!forced) {
       // If the sidebar is open and the tab is not active, just toggle every tab.
       $('#sidebar .tab').toggleClass('open')
       tab.trigger('opened')
